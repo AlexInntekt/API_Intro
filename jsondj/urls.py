f301d4efce5342bea869e62887e2f958c1addbf6
Matthew Freire <matthew@Matthews-MacBook-Pro.local>
2018-02-22 01:46:24 +0200

    Django Rest Framework intro

--- jsondj/urls.py
+++ jsondj/urls.py
@@ -1,14 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+from blog.api.views import BlogListAPIView
 from blog.views import blog_list, home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^home/', home),
     url(r'^$', blog_list),
-    url(r'^home', home)
+    url(r'^api/', BlogListAPIView.as_view())
+
 ]
 
 if settings.DEBUG:
--- static_in_env/main.js
+++ static_in_env/main.js
@@ -3,7 +3,7 @@
 
 var btn = document.getElementById("btn")
 var container = document.getElementById("ourcontainer")
-var url = 'http://127.0.0.1:8000/'
+var url = 'http://127.0.0.1:8000/api'
 
 $.ajax({
 	method: 'GET',
@@ -22,9 +22,33 @@ btn.addEventListener("click", function(){
 	var ourRequest = new XMLHttpRequest();
 	ourRequest.open("GET", url);
 	ourRequest.onload = function() {
-		console.log(ourRequest.responseText); // Not json formatted
 		var ourData = JSON.parse(ourRequest.responseText);
-		console.log(ourData);
+		renderHTML(ourData);
 	}
 	ourRequest.send();
+
 })
+
+
+function renderHTML(data) {
+	var container = document.getElementById("ourcontainer")
+	var htmlString = "";
+
+	for (i=0; i < data.length; i++){
+		htmlString += "<p>This items primary key is "  + data[i].pk + "</p>";
+	}
+
+	container.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
+
+
+
+
+
+
+
+
+
+
